0723d9e49e15d276ad0df947b52ad185046baf6c
Domen Kožar <domen@dev.si>
2012-03-17 13:03:04 +0100

    update docs

--- almir/lib/utils.py
+++ almir/lib/utils.py
@@ -1,22 +1,35 @@
 import os
+import time
 from docutils.core import publish_parts
 
 import pytz
 from pyramid.threadlocal import get_current_registry
 
 
-def convert_timezone(dt):
-    timezone = get_current_registry().settings['timezone']
-    return pytz.timezone(timezone).localize(dt)
+def convert_timezone(datetime):
+    """Converts datetime to timezone aware datetime.
+
+    Retrieving timezone:
+
+    - get `timezone` from .ini settings
+    - default to system timezone
+
+    """
+    timezone = get_current_registry().settings.get('timezone', None)
+    if not timezone:
+        timezone = time.tzname[0]
+    return pytz.timezone(timezone).localize(datetime)
 
 
 def timedelta_to_seconds(td):
+    """"""
     # http://docs.python.org/library/datetime.html?highlight=total_seconds#datetime.timedelta.total_seconds
     # to keep python2.6 support
     return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10 ** 6) / 10 ** 6
 
 
 def render_rst_section(filename):
+    """Finds filename in documentation directory and renders it to html."""
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'docs', 'source', filename)
     with open(path) as f:
         parts = publish_parts(f.read(), writer_name="html", settings_overrides={'initial_header_level': 2, 'doctitle_xform': False})
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -18,7 +18,7 @@ Welcome to almir's documentation!
 
     - supports bacula-director version 5.0.x
     - supports sqlite, postgresql, mysql databases
-    - complete representation of bacula-director information
+    - complete read-only interface for bacula-director information
     - interactive web console frontend to bconsole
     - export data to excel, pdf and clipboard 
     - supports python2.6 and python 2.7
