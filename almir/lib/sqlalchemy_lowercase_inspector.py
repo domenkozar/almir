456cc1b48a637dac1708797f3abb5a959eea320f
Domen Kožar <domen@dev.si>
2012-04-02 13:26:48 +0200

    remove unused reflection methods

--- almir/lib/sqlalchemy_lowercase_inspector.py
+++ almir/lib/sqlalchemy_lowercase_inspector.py
@@ -6,9 +6,9 @@ from sqlalchemy.engine.reflection import Inspector
 class LowerCaseInspector(Inspector):
     """Implements reflection inspector that reflects everything lowercase"""
 
-    def get_table_names(self, *a, **kw):
-        tables = super(LowerCaseInspector, self).get_table_names(*a, **kw)
-        return map(string.lower, tables)
+#    def get_table_names(self, *a, **kw):
+#        tables = super(LowerCaseInspector, self).get_table_names(*a, **kw)
+#        return map(string.lower, tables)
 
     def get_columns(self, *a, **kw):
         columns = super(LowerCaseInspector, self).get_columns(*a, **kw)
@@ -42,10 +42,6 @@ class LowerCaseInspector(Inspector):
         columns = filter(lambda x: x['referred_table'] != u'LocationId', columns)
 
         return map(lower_case, columns)
-
-    def get_primary_keys(self, *a, **kw):
-        columns = super(LowerCaseInspector, self).get_primary_keys(*a, **kw)
-        return map(string.lower, columns)
 
     def get_pk_constraint(self, *a, **kw):
         columns = super(LowerCaseInspector, self).get_pk_constraint(*a, **kw)
