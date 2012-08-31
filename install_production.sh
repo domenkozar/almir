26afc6e0f3596c2f761a0f35a7fa14b29414091b
Domen Kožar <domen@dev.si>
2012-08-31 18:05:57 +0200

    use printf instead of echo

--- install_production.sh
+++ install_production.sh
@@ -5,7 +5,7 @@ PYTHON_EXEC=$(which python2.7 || which python2.6)
 set -xe
 [ -z $PYTHON_EXEC ] && $(which echo) "No python2.7 or python2.6 binary found, exiting" && exit 1
 git clone https://github.com/iElectric/almir.git -b latests .
-$(which echo) -e '[buildout]\nextends = buildout.d/production.cfg' > buildout.cfg
+printf "[buildout]\nextends = buildout.d/production.cfg" > buildout.cfg
 $PYTHON_EXEC bootstrap.py
 bin/buildout
 bin/python almir/scripts/configure_deploy.py
