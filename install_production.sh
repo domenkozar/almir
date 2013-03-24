326275df3aae69afd5e5b166a0cf79e3e3d2d3cd
Domen Kožar <domen@dev.si>
2013-03-24 19:53:04 +0100

    try to fix #51

--- install_production.sh
+++ install_production.sh
@@ -6,7 +6,7 @@ set -xe
 [ -z $PYTHON_EXEC ] && $(which echo) "No python2.7 or python2.6 binary found, exiting" && exit 1
 git clone https://github.com/iElectric/almir.git -b latests .
 printf "[buildout]\nextends = buildout.d/production.cfg" > buildout.cfg
-$PYTHON_EXEC bootstrap.py
+$PYTHON_EXEC bootstrap.py -v 1.7.1
 bin/buildout
 bin/python almir/scripts/configure_deploy.py
 bin/buildout -o
